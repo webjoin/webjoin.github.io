d7441c36a8410c4833eaa040b6e149848b7bc94f
tangyu <tangyuningjin@163.com>
2023-06-27 22:52:39 +0800

    @日常消费记录出版@

---
+++ data.js
@@ -0,0 +1,30 @@
+
+var dailyColumns=["date","category","subCategory","goods","amount","currency","payer","memo"];
+
+var dailyDatas = [
+
+    {"date":"2023-06-26","category":"聚餐","subCategory":"聚餐","goods":"Qasr Alasala mandi and mathbi restaurant","amount":"165.00",currency:"AED","payer":"BaoLiangLu","memo":"With BaoLiangLu"},
+    {"date":"2023-06-25","category":"聚餐","subCategory":"聚餐","goods":"乾州火锅（烤肉）","amount":"390.00",currency:"AED","payer":"Elijah","memo":"With罗老师金晓姐小聚"},
+    {"date":"2023-06-24","category":"聚餐","subCategory":"礼物","goods":"粽子","amount":"84.00",currency:"AED","payer":"Jing","memo":"麦子团契礼物"},
+    {"date":"2023-06-24","category":"聚餐","subCategory":"聚餐","goods":"赢记（迪拜餐厅）","amount":"250.00",currency:"AED","payer":"Jing","memo":"麦子团契聚餐AA制"},
+
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人的机票","goods":"Jing's妈妈机票四川单程迪拜","amount":"4810.00",currency:"CNY","payer":"Elijah","memo":"机票"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人的机票","goods":"Jing's妈妈机票四川往返迪拜","amount":"11414.00",currency:"CNY","payer":"Elijah","memo":"机票"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人的机票","goods":"Elijah's爸爸哥哥机票武汉往返迪拜","amount":"8798.00",currency:"CNY","payer":"Elijah","memo":"机票"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人的机票","goods":"Elijah's嫂子机票武汉往返迪拜","amount":"5765.00",currency:"CNY","payer":"Elijah","memo":"机票"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人酒店","goods":"家人酒店","amount":"2925.00",currency:"AED","payer":"Elijah","memo":"家人酒店"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人旅行观光","goods":"Louvre","amount":"240.00",currency:"AED","payer":"Elijah","memo":"Elijah's爸爸&嫂子&Jing's妈妈"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人旅行观光","goods":"Sea World","amount":"1240.00",currency:"CNY","payer":"Jing","memo":"Elijah's爸爸&嫂子&Jing's妈妈"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"家人旅行观光","goods":"Ferrari Park","amount":"0.00",currency:"CNY","payer":"Jing","memo":"Elijah's Bro&Jing's Bro"},
+
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"婚礼场地的装饰","amount":"3200.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"专业摄影","amount":"2500.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"酒席餐费","amount":"18200.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"钻戒指5000迪","amount":"5000.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"对戒2800迪","amount":"2800.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"婚纱","amount":"1400.00",currency:"AED","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"淘宝买的小物品和礼品","amount":"3305.00",currency:"CNY","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"空运费","amount":"305.00",currency:"CNY","payer":"Jing","memo":"婚礼"},
+    {"date":"2023-05-20","category":"婚礼","subCategory":"婚礼","goods":"主内小礼品","amount":"424.00",currency:"CNY","payer":"Jing","memo":"婚礼"}
+]
+
